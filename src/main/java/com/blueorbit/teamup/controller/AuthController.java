ca8c504995ef16d8f65765b424af89f8111df3bd
YuzheLiu2002 <2000012956@stu.pku.edu.cn>
2022-11-06 19:01:02 +0800

    解决跨域问题

--- src/main/java/com/blueorbit/teamup/controller/AuthController.java
+++ src/main/java/com/blueorbit/teamup/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 public class AuthController {
@@ -14,6 +15,7 @@ public class AuthController {
     @Autowired
     private UserDao userDao;
 
+    @CrossOrigin
     @GetMapping("/login")
     public String login(@RequestParam("email") String email,
                           @RequestParam("password") String password){
@@ -30,6 +32,7 @@ public class AuthController {
         }
     }
 
+    @CrossOrigin
     @GetMapping("/register")
     public String register(@RequestParam("email") String email,
                         @RequestParam("password") String password){
