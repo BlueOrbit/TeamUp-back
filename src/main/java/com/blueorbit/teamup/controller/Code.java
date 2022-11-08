21d25ec23f29b1cb2ee44b1d4848e6ff61f9f3e7
BlueOrbit <alexlan60@outlook.com>
2022-11-08 11:25:40 +0800

    增加Result以规范化返回值

---
+++ src/main/java/com/blueorbit/teamup/controller/Code.java
@@ -0,0 +1,23 @@
+package com.blueorbit.teamup.controller;
+
+public class Code {
+    public static final Integer SAVE_USER_OK = 20011;
+    public static final Integer DELETE_USER_OK = 20021;
+    public static final Integer UPDATE_USER_OK = 20031;
+    public static final Integer GET_USER_OK = 20041;
+
+    public static final Integer SAVE_USER_ERR = 20010;
+    public static final Integer DELETE_USER_ERR = 20020;
+    public static final Integer UPDATE_USER_ERR = 20030;
+    public static final Integer GET_USER_ERR = 20040;
+
+    public static final Integer SAVE_TEAM_OK = 30011;
+    public static final Integer DELETE_TEAM_OK = 30021;
+    public static final Integer UPDATE_TEAM_OK = 30031;
+    public static final Integer GET_TEAM_OK = 30041;
+
+    public static final Integer SAVE_TEAM_ERR = 30010;
+    public static final Integer DELETE_TEAM_ERR = 30020;
+    public static final Integer UPDATE_TEAM_ERR = 30030;
+    public static final Integer GET_TEAM_ERR = 30040;
+}
