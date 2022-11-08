08e947993952308a79d52031697749cc244c1454
BlueOrbit <alexlan60@outlook.com>
2022-11-08 16:33:58 +0800

    完善Service类，准备将逻辑主体移动至业务层

---
+++ src/main/java/com/blueorbit/teamup/service/impl/TeamServiceImpl.java
@@ -0,0 +1,51 @@
+package com.blueorbit.teamup.service.impl;
+
+import com.blueorbit.teamup.domain.Team;
+import com.blueorbit.teamup.dao.TeamDao;
+import com.blueorbit.teamup.service.ITeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author BlueOrbit
+ * @since 2022-11-08
+ */
+@Service
+public class TeamServiceImpl  implements ITeamService {
+    @Autowired
+    private TeamDao teamDao;
+
+    @Override
+    public boolean save(Team team) {
+        teamDao.insert(team);
+        return true;
+    }
+
+    @Override
+    public boolean update(Team team) {
+        teamDao.updateById(team);
+        return true;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        teamDao.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public Team getById(Long id) {
+        return teamDao.selectById(id);
+    }
+
+    @Override
+    public List<Team> getAll() {
+        return teamDao.selectList(null);
+    }
+}
